6bcafdf64f0c9f75262bc95ed22fe8c734c3c104
aleix rodriguez <rodriguezaleix06@gmail.com>
2025-03-17 11:19:21 +0100

    Constructor que inicializa todos los atributos, incluyendo los de la superclase Vehicle

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,2 +1,9 @@
 public class Cotxe extends Vehicle {
     private int nombrePlaces; // Número de plazas del coche
+
+
+
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, Roda rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.nombrePlaces = nombrePlaces;
+    }
