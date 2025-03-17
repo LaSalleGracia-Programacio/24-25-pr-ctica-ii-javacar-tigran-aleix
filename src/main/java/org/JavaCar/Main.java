6dfb736f2bc657c8d7f0655b7f4b6da095a58b10
aleix rodriguez <rodriguezaleix06@gmail.com>
2025-03-17 10:33:16 +0100

    Crear instancias de vehículos

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -8,3 +8,8 @@ public class Main {
         Motor motorElectrico = new Motor("Eléctrico", 100);
         Roda rodaMichelin = new Roda("Michelin", 17.5);
         Roda rodaPirelli = new Roda("Pirelli", 16.0);
+
+        
+        Cotxe cotxe1 = new Cotxe("1234ABC", "Toyota", "Corolla", 30.0, 5, motorGasolina, rodaMichelin);
+        Moto moto1 = new Moto("5678XYZ", "Honda", "CBR600", 25.0, 600, motorGasolina, rodaPirelli);
+        Furgoneta furgoneta1 = new Furgoneta("9101DEF", "Mercedes", "Sprinter", 50.0, 1200, motorElectrico, rodaMichelin);
